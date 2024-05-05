9d49e9217bf9694b56f2c726672bdd4215ea9888
dubstabber <taesan@protonmail.com>
2024-05-05 21:45:59 +0200

    Add new map modal window, theme and xolonium font

--- MenuBar.gd
+++ MenuBar.gd
@@ -4,6 +4,10 @@ var submenu1: PopupMenu = PopupMenu.new()
 var submenu2: PopupMenu = PopupMenu.new()
 
 @onready var file_menu = $File
+@onready var view_menu = $View
+
+@onready var new_map_window = $"../NewMapWindow"
+
 
 func _ready():
 	file_menu.add_item("New map")
@@ -19,6 +23,10 @@ func _ready():
 	file_menu.add_submenu_item("submenuitem", "submenu")
 	submenu1.add_submenu_item("subermenu", "submenu2")
 
+	file_menu.connect("index_pressed", _on_file_menu_pressed)
 
-func _process(_delta):
-	pass
+
+func _on_file_menu_pressed(id: int):
+	match id:
+		0:
+			new_map_window.show()
