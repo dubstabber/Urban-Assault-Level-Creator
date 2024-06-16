af8ed7a3de979433880035ad12dc86066ff81212
dubstabber <taesan@protonmail.com>
2024-06-16 17:12:02 +0200

    Cleaner code

--- scenes/squad.gd
+++ scenes/squad.gd
@@ -1,27 +1,33 @@
 extends Unit
 
+var map_icon: String
+
 var num := 1
 var useable := false
 
 
 func create(_owner_id: int, squad_data: Dictionary):
-	owner_id = _owner_id
 	vehicle = squad_data.id
-	match owner_id:
-		1:
-			texture = CurrentMapData.squad_images[squad_data.mapIcon].blue
-		2:
-			texture = CurrentMapData.squad_images[squad_data.mapIcon].green
-		3:
-			texture = CurrentMapData.squad_images[squad_data.mapIcon].white
-		4:
-			texture = CurrentMapData.squad_images[squad_data.mapIcon].yellow
-		5:
-			texture = CurrentMapData.squad_images[squad_data.mapIcon].gray
-		6:
-			texture = CurrentMapData.squad_images[squad_data.mapIcon].red
-		7:
-			texture = CurrentMapData.squad_images["square"].red2
-			
+	map_icon = squad_data.mapIcon
+	change_faction(_owner_id)
 	button.position -= Vector2(texture.get_width()/2.0, texture.get_height()/2.0)
 	button.size = Vector2(texture.get_width(), texture.get_height())
+
+
+func change_faction(_owner_id: int):
+	owner_id = _owner_id
+	match owner_id:
+		1:
+			texture = CurrentMapData.squad_images[map_icon].blue
+		2:
+			texture = CurrentMapData.squad_images[map_icon].green
+		3:
+			texture = CurrentMapData.squad_images[map_icon].white
+		4:
+			texture = CurrentMapData.squad_images[map_icon].yellow
+		5:
+			texture = CurrentMapData.squad_images[map_icon].gray
+		6:
+			texture = CurrentMapData.squad_images[map_icon].red
+		7:
+			texture = CurrentMapData.squad_images["square"].red2
