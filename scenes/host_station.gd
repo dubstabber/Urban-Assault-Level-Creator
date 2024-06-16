9c10dca7726c9ead1550597a96f00bb6a1174d16
dubstabber <taesan@protonmail.com>
2024-06-16 16:59:33 +0200

    Add missing data to host station structure, add default values

--- scenes/host_station.gd
+++ scenes/host_station.gd
@@ -2,22 +2,24 @@ extends Unit
 
 var pos_y := 500
 var energy := 460000
-var con_budget: int
-var con_delay: int
-var def_budget: int
-var def_delay: int
-var rec_budget: int
-var rec_delay: int
-var rob_budget: int
-var rob_delay: int
-var pow_budget: int
-var pow_delay: int
-var rad_budget: int
-var rad_delay: int
-var saf_budget: int
-var saf_delay: int
-var cpl_budget: int
-var cpl_delay: int
+var view_angle := -1
+var reload_const := -1
+var con_budget := 30
+var con_delay := 0
+var def_budget := 90
+var def_delay := 0
+var rec_budget := 80
+var rec_delay := 0
+var rob_budget := 100
+var rob_delay := 0
+var pow_budget := 50
+var pow_delay := 0
+var rad_budget := 30
+var rad_delay := 0
+var saf_budget := 50
+var saf_delay := 0
+var cpl_budget := 100
+var cpl_delay := 0
 
 
 func create(_owner_id, _vehicle):
