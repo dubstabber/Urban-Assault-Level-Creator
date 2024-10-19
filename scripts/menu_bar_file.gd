78d584daafea7e4c3d8f98f7e7afd2df4339e500
dubstabber <taesan@protonmail.com>
2024-10-19 17:32:25 +0200

    Move file menuBar script to File component

--- scripts/menu_bar_file.gd
+++ scripts/menu_bar_file.gd
@@ -1,29 +1,25 @@
-extends MenuBar
-
-var submenu1: PopupMenu = PopupMenu.new()
-var submenu2: PopupMenu = PopupMenu.new()
-
-@onready var file_menu = $File
-@onready var view_menu = $View
+extends PopupMenu
 
 @onready var new_map_window = %NewMapWindow
 
 
 func _ready():
-	file_menu.add_item("New map")
+	add_item("New map")
 	
+	var submenu1: PopupMenu = PopupMenu.new()
 	submenu1.name = "submenu"
-	file_menu.add_child(submenu1)
+	add_child(submenu1)
 	submenu1.add_item("sub item 1")
 	
+	var submenu2: PopupMenu = PopupMenu.new()
 	submenu2.name = "submenu2"
 	submenu1.add_child(submenu2)
 	submenu2.add_item("The newer item")
 	
-	file_menu.add_submenu_item("submenuitem", "submenu")
+	add_submenu_item("submenuitem", "submenu")
 	submenu1.add_submenu_item("subermenu", "submenu2")
 
-	file_menu.connect("index_pressed", _on_file_menu_pressed)
+	connect("index_pressed", _on_file_menu_pressed)
 
 
 func _on_file_menu_pressed(id: int):
