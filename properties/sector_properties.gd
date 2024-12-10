62d28f33b22536ae0f26b1fba34d691b0f8c474d
dubstabber <taesan@protonmail.com>
2024-12-10 16:47:51 +0100

    Selected sector border is now indicated in the properties panel

--- properties/sector_properties.gd
+++ properties/sector_properties.gd
@@ -17,6 +17,25 @@ func _update_properties() -> void:
 		%SectorPropertiesContainer.show()
 		%SectorPositionLabel.text = "Selected sector X: %s Y: %s" % [CurrentMapData.selected_sector.x, CurrentMapData.selected_sector.y]
 		
+		if (CurrentMapData.selected_sector.x == 0 or CurrentMapData.selected_sector.y == 0 or
+			CurrentMapData.selected_sector.x == (CurrentMapData.horizontal_sectors+1) or
+			CurrentMapData.selected_sector.y == (CurrentMapData.vertical_sectors+1)):
+			%BorderInfoLabel.show()
+			$ScrollContainer/MarginContainer/SectorPropertiesContainer/GridContainer.hide()
+			$ScrollContainer/MarginContainer/SectorPropertiesContainer/HSeparator.hide()
+			$ScrollContainer/MarginContainer/SectorPropertiesContainer/HSeparator2.hide()
+			%BuildingTextLabel.hide()
+			%BuildingTexture.hide()
+			%InvalidTypMapLabel.hide()
+			return
+		else:
+			%BorderInfoLabel.hide()
+			$ScrollContainer/MarginContainer/SectorPropertiesContainer/GridContainer.show()
+			$ScrollContainer/MarginContainer/SectorPropertiesContainer/HSeparator.show()
+			$ScrollContainer/MarginContainer/SectorPropertiesContainer/HSeparator2.show()
+			%BuildingTextLabel.show()
+			%BuildingTexture.show()
+		
 		if CurrentMapData.own_map[CurrentMapData.selected_sector_idx] == 0:
 			%SectorOwnerLabel.text = "Neutral"
 		else:
