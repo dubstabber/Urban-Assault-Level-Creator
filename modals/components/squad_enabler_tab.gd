02ef3600ff068ffaaa27a4d311a3c6605a1ef702
dubstabber <taesan@protonmail.com>
2025-01-03 19:40:03 +0100

    Update unit and building names, update items in squad enabler

--- modals/components/squad_enabler_tab.gd
+++ modals/components/squad_enabler_tab.gd
@@ -1,6 +1,5 @@
 extends TabBar
 
-
 @export var ITEM_CHECK_BOX_CONTAINER: PackedScene
 @export var host_station_owner_id: int
 
@@ -17,7 +16,8 @@ func refresh() -> void:
 	for child in %SulgogarUnitsContainer.get_children(): child.queue_free()
 	for child in %BlackSectBuildingsContainer.get_children(): child.queue_free()
 	for child in %TrainingUnitsContainer.get_children(): child.queue_free()
-	for child in %SpecialUnitsContainer.get_children(): child.queue_free()
+	for child in %MiscUnitsContainer.get_children(): child.queue_free()
+	for child in %MiscBuildingsContainer.get_children(): child.queue_free()
 	
 	if ITEM_CHECK_BOX_CONTAINER == null: return
 	for hs in Preloads.ua_data.data[EditorState.game_data_type].hoststations:
@@ -58,7 +58,16 @@ func refresh() -> void:
 		item_check_box.owner_id = host_station_owner_id
 		item_check_box.item_id = squad.id
 		item_check_box.change_button_availability(true, 0)
-		%SpecialUnitsContainer.add_child(item_check_box)
+		%MiscUnitsContainer.add_child(item_check_box)
+	for building in Preloads.ua_data.data[EditorState.game_data_type].other.buildings:
+		var item_check_box = ITEM_CHECK_BOX_CONTAINER.instantiate()
+		item_check_box.item_texture = Preloads.building_icons[building.icon_type]
+		item_check_box.label_text = building.name
+		item_check_box.item_type = "building"
+		item_check_box.owner_id = host_station_owner_id
+		item_check_box.item_id = building.id
+		item_check_box.change_button_availability(true, 0)
+		%MiscBuildingsContainer.add_child(item_check_box)
 	%UnlockItemsCheckBox.button_pressed = false
 
 
@@ -85,5 +94,7 @@ func _on_unlock_items_check_box_toggled(toggled_on: bool) -> void:
 			item_check_box.change_button_availability(not toggled_on, 5)
 		for item_check_box in %TrainingUnitsContainer.get_children():
 			item_check_box.change_button_availability(not toggled_on, 7)
-		for item_check_box in %SpecialUnitsContainer.get_children():
+		for item_check_box in %MiscUnitsContainer.get_children():
+			item_check_box.change_button_availability(not toggled_on, 0)
+		for item_check_box in %MiscBuildingsContainer.get_children():
 			item_check_box.change_button_availability(not toggled_on, 0)
