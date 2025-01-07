8c5b0dd1fa13008298c13c7dd4e98d72af576bb4
dubstabber <taesan@protonmail.com>
2025-01-07 19:33:19 +0100

    Fix saving indicator

--- modals/mission_briefing_maps_window.gd
+++ modals/mission_briefing_maps_window.gd
@@ -40,14 +40,16 @@ func get_option_index_by_text(option_button: OptionButton, text: String) -> int:
 
 func _on_briefing_maps_option_button_item_selected(index: int) -> void:
 	var item_text = %BriefingMapsOptionButton.get_item_text(index)
+	if CurrentMapData.briefing_map != item_text: CurrentMapData.is_saved = false
 	CurrentMapData.briefing_map = item_text
 	var mb_map_name = item_text.replace(".%s" %item_text.get_extension(), "")
 	%BriefingMapTexture.texture = Preloads.mbmaps[mb_map_name]
-	CurrentMapData.is_saved = false
+	
 
 
 func _on_debriefing_maps_option_button_item_selected(index: int) -> void:
 	var item_text = %DebriefingMapsOptionButton.get_item_text(index)
+	if CurrentMapData.debriefing_map != item_text: CurrentMapData.is_saved = false
 	CurrentMapData.debriefing_map = item_text
 	var db_map_name = item_text.replace(".%s" %item_text.get_extension(), "")
 	%DebriefingMapTexture.texture = Preloads.mbmaps[db_map_name]
@@ -59,20 +61,25 @@ func close() -> void:
 
 
 func _on_m_bsize_x_spin_box_value_changed(value: float) -> void:
+	if CurrentMapData.briefing_size_x != int(value):
+		CurrentMapData.is_saved = false
 	CurrentMapData.briefing_size_x = int(value)
-	CurrentMapData.is_saved = false
 
 
 func _on_m_bsize_y_spin_box_value_changed(value: float) -> void:
+	if CurrentMapData.briefing_size_y != int(value):
+		CurrentMapData.is_saved = false
 	CurrentMapData.briefing_size_y = int(value)
-	CurrentMapData.is_saved = false
 
 
 func _on_d_bsize_x_spin_box_value_changed(value: float) -> void:
+	if CurrentMapData.debriefing_size_x != int(value):
+		CurrentMapData.is_saved = false
 	CurrentMapData.debriefing_size_x = int(value)
-	CurrentMapData.is_saved = false
 
 
 func _on_d_bsize_y_spin_box_value_changed(value: float) -> void:
+	if CurrentMapData.debriefing_size_y != int(value):
+		CurrentMapData.is_saved = false
 	CurrentMapData.debriefing_size_y = int(value)
-	CurrentMapData.is_saved = false
+	
