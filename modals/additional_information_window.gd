306709d12bd06a0ad3571593f311c15284eaf074
dubstabber <taesan@protonmail.com>
2025-02-19 19:26:18 +0100

    Add additional informations

--- modals/additional_information_window.gd
+++ modals/additional_information_window.gd
@@ -1,7 +1,10 @@
 extends Window
 
+const level_builder_document_path = "res://resources/Levelbuilder_English.doc"
+
 
 func _ready() -> void:
+	%InformationList.select(0)
 	%InformationList.item_selected.connect(func(index:int):
 		for child in %InformationContainer.get_children():
 			child.hide()
