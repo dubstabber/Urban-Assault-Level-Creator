4836a57af231f0fb6d2f511a6c7d6ed1e8434f1c
dubstabber <taesan@protonmail.com>
2025-02-19 20:59:51 +0100

    Add information with document on how to build a level via text file

--- modals/additional_information_window.gd
+++ modals/additional_information_window.gd
@@ -1,6 +1,6 @@
 extends Window
 
-const level_builder_document_path = "res://resources/Levelbuilder_English.doc"
+const LEVEL_BUILDER_DOCUMENT_PATH = "res://resources/Levelbuilder_English.doc"
 
 
 func _ready() -> void:
@@ -10,7 +10,28 @@ func _ready() -> void:
 			child.hide()
 		%InformationContainer.get_child(index).show()
 	)
+	
 
 
 func close() -> void:
 	hide()
+
+
+func _on_level_builder_file_label_meta_clicked(meta: Variant) -> void:
+	if meta == "level_builder_doc":
+		var file = FileAccess.open(LEVEL_BUILDER_DOCUMENT_PATH, FileAccess.READ)
+		if not file: 
+			printerr("Failed to load embedded .doc file.")
+			return
+		var buffer = file.get_buffer(file.get_length())
+		file.close()
+		
+		var temp_path = OS.get_cache_dir() + "/Levelbuilder_English.doc"
+		var temp_file = FileAccess.open(temp_path, FileAccess.WRITE)
+		if not temp_file: 
+			printerr("Failed to write temporary file.")
+			return
+		temp_file.store_buffer(buffer)
+		temp_file.close()
+		
+		OS.shell_open(temp_path)
