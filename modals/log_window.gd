1b766a7776ca0506d7c36c348a35f9e84c86b9b5
dubstabber <taesan@protonmail.com>
2025-02-26 15:39:32 +0100

    Implement warning logs system

--- modals/log_window.gd
+++ modals/log_window.gd
@@ -1,24 +1,32 @@
 extends Window
 
-@onready var logs_container: VBoxContainer = %LogsContainer
+@export var warning_icon: CompressedTexture2D
+@onready var logs_list: ItemList = %LogsList
 
 
 func _ready() -> void:
 	EventSystem.warning_logs_window_requested.connect(display_messages)
+	logs_list.clear()
 
 
 func display_messages() -> void:
 	for message in EditorState.warning_messages:
-		var new_label = Label.new()
-		new_label.text = message
-		new_label["theme_override_colors/font_color"] = "b67b00"
-		new_label["theme_override_font_sizes/font_size"] = 14
-		logs_container.add_child(new_label)
+		logs_list.add_item(message, warning_icon)
 	popup()
 
 
 func close() -> void:
-	for child in logs_container.get_children():
-		logs_container.remove_child(child)
-		child.queue_free()
+	logs_list.clear()
 	hide()
+
+
+func _on_refresh_button_pressed() -> void:
+	EditorState.refresh_warnings()
+	logs_list.clear()
+	display_messages()
+
+
+func _on_clear_button_pressed() -> void:
+	EditorState.warning_messages.clear()
+	logs_list.clear()
+	EventSystem.warning_logs_updated.emit()
