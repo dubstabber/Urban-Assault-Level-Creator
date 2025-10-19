69cda9f114f9f326f4e1482ff2fd7e8b57e8ac75
dubstabber <taesan@protonmail.com>
2025-10-19 19:39:13 +0200

    Fix delay conversion units in host station section and behaviour loader

--- properties/unit_components/host_station/host_station_properties.gd
+++ properties/unit_components/host_station/host_station_properties.gd
@@ -115,7 +115,7 @@ func _ready() -> void:
 		conquering_value_label.text = str(value_changed)
 	)
 	conquering_delay_line_edit.text_changed.connect(func(value_changed: String):
-		var converted_value = abs(int(value_changed)) * 1000
+		var converted_value = abs(int(value_changed)) * 1024
 		if EditorState.selected_unit.con_delay != converted_value:
 			CurrentMapData.is_saved = false
 			EditorState.selected_unit.con_delay = converted_value
@@ -127,7 +127,7 @@ func _ready() -> void:
 		defense_value_label.text = str(value_changed)
 	)
 	defense_delay_line_edit.text_changed.connect(func(value_changed: String):
-		var converted_value = abs(int(value_changed)) * 1000
+		var converted_value = abs(int(value_changed)) * 1024
 		if EditorState.selected_unit.def_delay != converted_value:
 			CurrentMapData.is_saved = false
 			EditorState.selected_unit.def_delay = converted_value
@@ -139,7 +139,7 @@ func _ready() -> void:
 		reconnaissance_value_label.text = str(value_changed)
 	)
 	reconnaissance_delay_line_edit.text_changed.connect(func(value_changed: String):
-		var converted_value = abs(int(value_changed)) * 1000
+		var converted_value = abs(int(value_changed)) * 1024
 		if EditorState.selected_unit.rec_delay != converted_value:
 			CurrentMapData.is_saved = false
 			EditorState.selected_unit.rec_delay = converted_value
@@ -151,7 +151,7 @@ func _ready() -> void:
 		attacking_value_label.text = str(value_changed)
 	)
 	attacking_delay_line_edit.text_changed.connect(func(value_changed: String):
-		var converted_value = abs(int(value_changed)) * 1000
+		var converted_value = abs(int(value_changed)) * 1024
 		if EditorState.selected_unit.rob_delay != converted_value:
 			CurrentMapData.is_saved = false
 			EditorState.selected_unit.rob_delay = converted_value
@@ -163,7 +163,7 @@ func _ready() -> void:
 		power_building_value_label.text = str(value_changed)
 	)
 	power_building_delay_line_edit.text_changed.connect(func(value_changed: String):
-		var converted_value = abs(int(value_changed)) * 1000
+		var converted_value = abs(int(value_changed)) * 1024
 		if EditorState.selected_unit.pow_delay != converted_value:
 			CurrentMapData.is_saved = false
 			EditorState.selected_unit.pow_delay = converted_value
@@ -175,7 +175,7 @@ func _ready() -> void:
 		radar_building_value_label.text = str(value_changed)
 	)
 	radar_building_delay_line_edit.text_changed.connect(func(value_changed: String):
-		var converted_value = abs(int(value_changed)) * 1000
+		var converted_value = abs(int(value_changed)) * 1024
 		if EditorState.selected_unit.rad_delay != converted_value:
 			CurrentMapData.is_saved = false
 			EditorState.selected_unit.rob_delay = converted_value
@@ -187,7 +187,7 @@ func _ready() -> void:
 		power_building_value_label.text = str(value_changed)
 	)
 	power_building_delay_line_edit.text_changed.connect(func(value_changed: String):
-		var converted_value = abs(int(value_changed)) * 1000
+		var converted_value = abs(int(value_changed)) * 1024
 		if EditorState.selected_unit.pow_delay != converted_value:
 			CurrentMapData.is_saved = false
 			EditorState.selected_unit.pow_delay = converted_value
@@ -199,7 +199,7 @@ func _ready() -> void:
 		radar_building_value_label.text = str(value_changed)
 	)
 	radar_building_delay_line_edit.text_changed.connect(func(value_changed: String):
-		var converted_value = abs(int(value_changed)) * 1000
+		var converted_value = abs(int(value_changed)) * 1024
 		if EditorState.selected_unit.rad_delay != converted_value:
 			CurrentMapData.is_saved = false
 			EditorState.selected_unit.rad_delay = converted_value
@@ -211,7 +211,7 @@ func _ready() -> void:
 		flak_building_value_label.text = str(value_changed)
 	)
 	flak_building_delay_line_edit.text_changed.connect(func(value_changed: String):
-		var converted_value = abs(int(value_changed)) * 1000
+		var converted_value = abs(int(value_changed)) * 1024
 		if EditorState.selected_unit.saf_delay != converted_value:
 			CurrentMapData.is_saved = false
 			EditorState.selected_unit.saf_delay = converted_value
@@ -223,7 +223,7 @@ func _ready() -> void:
 		moving_station_value_label.text = str(value_changed)
 	)
 	moving_station_delay_line_edit.text_changed.connect(func(value_changed: String):
-		var converted_value = abs(int(value_changed)) * 1000
+		var converted_value = abs(int(value_changed)) * 1024
 		if EditorState.selected_unit.cpl_delay != converted_value:
 			CurrentMapData.is_saved = false
 			EditorState.selected_unit.cpl_delay = converted_value
@@ -253,49 +253,49 @@ func _ready() -> void:
 			conquering_h_slider.value = int(behavior_data.con_budget)
 		if behavior_data.has("con_delay"):
 			EditorState.selected_unit.con_delay = behavior_data.con_delay
-			conquering_delay_line_edit.text = str(behavior_data.con_delay)
+			conquering_delay_line_edit.text = str(int(behavior_data.con_delay/1024.0))
 		if behavior_data.has("def_budget"):
 			EditorState.selected_unit.def_budget = behavior_data.def_budget
 			defense_h_slider.value = int(behavior_data.def_budget)
 		if behavior_data.has("def_delay"):
 			EditorState.selected_unit.def_delay = behavior_data.def_delay
-			defense_delay_line_edit.text = str(behavior_data.def_delay)
+			defense_delay_line_edit.text = str(int(behavior_data.def_delay/1024.0))
 		if behavior_data.has("rec_budget"):
 			EditorState.selected_unit.rec_budget = behavior_data.rec_budget
 			reconnaissance_h_slider.value = int(behavior_data.rec_budget)
 		if behavior_data.has("rec_delay"):
 			EditorState.selected_unit.rec_delay = behavior_data.rec_delay
-			reconnaissance_delay_line_edit.text = str(behavior_data.rec_delay)
+			reconnaissance_delay_line_edit.text = str(int(behavior_data.rec_delay/1024.0))
 		if behavior_data.has("rob_budget"):
 			EditorState.selected_unit.rob_budget = behavior_data.rob_budget
 			attacking_h_slider.value = int(behavior_data.rob_budget)
 		if behavior_data.has("rob_delay"):
 			EditorState.selected_unit.rob_delay = behavior_data.rob_delay
-			attacking_delay_line_edit.text = str(behavior_data.rob_delay)
+			attacking_delay_line_edit.text = str(int(behavior_data.rob_delay/1024.0))
 		if behavior_data.has("pow_budget"):
 			EditorState.selected_unit.pow_budget = behavior_data.pow_budget
 			power_building_h_slider.value = int(behavior_data.pow_budget)
 		if behavior_data.has("pow_delay"):
 			EditorState.selected_unit.pow_delay = behavior_data.pow_delay
-			power_building_delay_line_edit.text = str(behavior_data.pow_delay)
+			power_building_delay_line_edit.text = str(int(behavior_data.pow_delay/1024.0))
 		if behavior_data.has("rad_budget"):
 			EditorState.selected_unit.rad_budget = behavior_data.rad_budget
 			radar_building_h_slider.value = int(behavior_data.rad_budget)
 		if behavior_data.has("rad_delay"):
 			EditorState.selected_unit.rad_delay = behavior_data.rad_delay
-			radar_building_delay_line_edit.text = str(behavior_data.rad_delay)
+			radar_building_delay_line_edit.text = str(int(behavior_data.rad_delay/1024.0))
 		if behavior_data.has("saf_budget"):
 			EditorState.selected_unit.saf_budget = behavior_data.saf_budget
 			flak_building_h_slider.value = int(behavior_data.saf_budget)
 		if behavior_data.has("saf_delay"):
 			EditorState.selected_unit.saf_delay = behavior_data.saf_delay
-			flak_building_delay_line_edit.text = str(behavior_data.saf_delay)
+			flak_building_delay_line_edit.text = str(int(behavior_data.saf_delay/1024.0))
 		if behavior_data.has("cpl_budget"):
 			EditorState.selected_unit.cpl_budget = behavior_data.cpl_budget
 			moving_station_h_slider.value = int(behavior_data.cpl_budget)
 		if behavior_data.has("cpl_delay"):
 			EditorState.selected_unit.cpl_delay = behavior_data.cpl_delay
-			moving_station_delay_line_edit.text = str(behavior_data.cpl_delay)
+			moving_station_delay_line_edit.text = str(int(behavior_data.cpl_delay/1024.0))
 		CurrentMapData.is_saved = false
 	)
 
@@ -334,28 +334,28 @@ func _update_properties() -> void:
 		
 		conquering_h_slider.value = EditorState.selected_unit.con_budget
 		conquering_value_label.text = str(EditorState.selected_unit.con_budget)
-		conquering_delay_line_edit.text = str(EditorState.selected_unit.con_delay / 1000)
+		conquering_delay_line_edit.text = str(EditorState.selected_unit.con_delay / 1024)
 		defense_h_slider.value = EditorState.selected_unit.def_budget
 		defense_value_label.text = str(EditorState.selected_unit.def_budget)
-		defense_delay_line_edit.text = str(EditorState.selected_unit.def_delay / 1000)
+		defense_delay_line_edit.text = str(EditorState.selected_unit.def_delay / 1024)
 		reconnaissance_h_slider.value = EditorState.selected_unit.rec_budget
 		reconnaissance_value_label.text = str(EditorState.selected_unit.rec_budget)
-		reconnaissance_delay_line_edit.text = str(EditorState.selected_unit.rec_delay / 1000)
+		reconnaissance_delay_line_edit.text = str(EditorState.selected_unit.rec_delay / 1024)
 		attacking_h_slider.value = EditorState.selected_unit.rob_budget
 		attacking_value_label.text = str(EditorState.selected_unit.rob_budget)
-		attacking_delay_line_edit.text = str(EditorState.selected_unit.rob_delay / 1000)
+		attacking_delay_line_edit.text = str(EditorState.selected_unit.rob_delay / 1024)
 		power_building_h_slider.value = EditorState.selected_unit.pow_budget
 		power_building_value_label.text = str(EditorState.selected_unit.pow_budget)
-		power_building_delay_line_edit.text = str(EditorState.selected_unit.pow_delay / 1000)
+		power_building_delay_line_edit.text = str(EditorState.selected_unit.pow_delay / 1024)
 		radar_building_h_slider.value = EditorState.selected_unit.rad_budget
 		radar_building_value_label.text = str(EditorState.selected_unit.rad_budget)
-		radar_building_delay_line_edit.text = str(EditorState.selected_unit.rad_delay / 1000)
+		radar_building_delay_line_edit.text = str(EditorState.selected_unit.rad_delay / 1024)
 		flak_building_h_slider.value = EditorState.selected_unit.saf_budget
 		flak_building_value_label.text = str(EditorState.selected_unit.saf_budget)
-		flak_building_delay_line_edit.text = str(EditorState.selected_unit.saf_delay / 1000)
+		flak_building_delay_line_edit.text = str(EditorState.selected_unit.saf_delay / 1024)
 		moving_station_h_slider.value = EditorState.selected_unit.cpl_budget
 		moving_station_value_label.text = str(EditorState.selected_unit.cpl_budget)
-		moving_station_delay_line_edit.text = str(EditorState.selected_unit.cpl_delay / 1000)
+		moving_station_delay_line_edit.text = str(EditorState.selected_unit.cpl_delay / 1024)
 		
 		mb_status_host_station_check_box.button_pressed = EditorState.selected_unit.mb_status
 		
