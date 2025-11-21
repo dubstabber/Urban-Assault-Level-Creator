37cb93b124da2a7ede1bb4354ad6df6489681a6e
dubstabber <taesan@protonmail.com>
2025-11-21 21:44:35 +0100

    Upgrade project to godot 4.5.1, fix modal blocked input bug(I hope)

--- modals/modals.gd
+++ modals/modals.gd
@@ -1,18 +1,42 @@
 extends Node
 
 
+# Workaround for Godot 4.4+ exclusive window focus bug
+# When showing exclusive windows, we need to explicitly request focus to prevent input blocking
+func _show_modal_window(window: Window) -> void:
+	window.popup()
+	# Small delay to ensure window is properly initialized before grabbing focus
+	await get_tree().process_frame
+	# Find first focusable child and grab focus
+	var focusable = _find_focusable_child(window)
+	if focusable:
+		focusable.grab_focus()
+	else:
+		window.grab_focus()
+
+
+func _find_focusable_child(node: Node) -> Control:
+	if node is Control and node.focus_mode != Control.FOCUS_NONE:
+		return node as Control
+	for child in node.get_children():
+		var result = _find_focusable_child(child)
+		if result:
+			return result
+	return null
+
+
 func _ready() -> void:
 	EventSystem.new_map_requested.connect(func():
 		if not CurrentMapData.is_saved:
 			%UnsavedMapConfirmationDialog.show()
 			await %UnsavedMapConfirmationDialog.close_requested
-		%NewMapWindow.popup()
+		await _show_modal_window(%NewMapWindow)
 	)
 	EventSystem.open_map_requested.connect(func():
 		if not CurrentMapData.is_saved:
 			%UnsavedMapConfirmationDialog.show()
 			await %UnsavedMapConfirmationDialog.close_requested
-		%OpenLevelFileDialog.popup()
+		await _show_modal_window(%OpenLevelFileDialog)
 		)
 	EventSystem.open_map_drag_requested.connect(func(file_path: String):
 		if not CurrentMapData.is_saved:
@@ -26,14 +50,14 @@ func _ready() -> void:
 		if CurrentMapData.horizontal_sectors == 0 or CurrentMapData.vertical_sectors == 0:
 			return
 		if CurrentMapData.map_path.is_empty():
-			%SaveLevelFileDialog.popup()
+			await _show_modal_window(%SaveLevelFileDialog)
 		else:
 			SingleplayerSaver.save()
 		)
 	EventSystem.save_as_map_requested.connect(func():
 		if CurrentMapData.horizontal_sectors == 0 or CurrentMapData.vertical_sectors == 0:
 			return
-		%SaveLevelFileDialog.popup()
+		await _show_modal_window(%SaveLevelFileDialog)
 		)
 	EventSystem.close_map_requested.connect(func():
 		if not CurrentMapData.is_saved:
