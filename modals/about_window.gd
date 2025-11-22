833d3f1053168121be38cbbafb09d28b44a7aad1
dubstabber <taesan@protonmail.com>
2025-11-22 13:15:29 +0100

    Various minor improvements

--- modals/about_window.gd
+++ modals/about_window.gd
@@ -1,5 +1,13 @@
 extends Window
 
 
+@onready var version_label: Label = %VersionLabel
+
+
+func _ready() -> void:
+	var version = ProjectSettings.get_setting("application/config/version", "1.0.0")
+	version_label.text = "Version %s" % version
+
+
 func close() -> void:
 	hide()
